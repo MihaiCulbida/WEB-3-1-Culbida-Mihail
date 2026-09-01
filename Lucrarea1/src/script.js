09f789c715a002fd0366018717adb4c32350db8a
MihaiCulbida <clbidamihai@gmail.com>
2026-09-01 11:13:15 +0300

    Added a btn with console.log function

--- Lucrarea1/src/script.js
+++ Lucrarea1/src/script.js
@@ -1,1 +1,7 @@
 console.log("Hell World");
+
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+    console.log("A fost apasat butonul type");
+});
