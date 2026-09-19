9f1c0f9cb8c189b12aa389cd00fb65037b34de61
MihaiCulbida <clbidamihai@gmail.com>
2026-09-19 13:06:22 +0300

    Second functional task on js

--- Lucrarea3/src/script.js
+++ Lucrarea3/src/script.js
@@ -5,3 +5,18 @@ function calculateSum(a, b) {
 
 console.log(calculateSum(2, 8));
 console.log(calculateSum(600, 400));
+
+const student = {
+    name: "Mihail",
+    age: 17,
+    grade: 2,
+};
+
+function introduce() {
+    console.log("Salut, numele meu este " + student.name + ", am " + student.age + " ani și am nota " + student.grade);
+}
+
+student.introduce = introduce;
+student.introduce();
+student.grade = 10;
+console.log(student.grade);
